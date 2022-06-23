0c9c9440b97f07864782e01378daeedaec606533
Roman M. Gerasimov <romagrizly@gmail.com>
2022-06-23 15:15:00 +0500

    refactor: task endpoint

--- services/lessons_tasks_service.py
+++ services/lessons_tasks_service.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from database import LessonsTasks
 
@@ -14,5 +17,14 @@ class LessonsTasksService:
                                              LessonsTasks.task_id == task_id))
         lesson_task = query.scalars().first()
         return lesson_task
+
+    @staticmethod
+    async def get_lesson_tasks(lesson_id: int,
+                               session: AsyncSession) -> List[LessonsTasks]:
+        query = await session.execute(select(LessonsTasks)
+                                      .where(LessonsTasks.lesson_id == lesson_id)
+                                      .options(joinedload(LessonsTasks.task)))
+        lesson_tasks = query.scalars().all()
+        return lesson_tasks
 
 
